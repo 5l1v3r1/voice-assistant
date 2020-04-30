dc63edd52a69ce17712124c5f535bbb7fead4895
H3ll C0me <64320083+H3llC0me@users.noreply.github.com>
2020-04-30 22:05:18 +0300

    Create allvoices.py

---
+++ allvoices.py
@@ -0,0 +1,62 @@
+import os
+import datetime
+import locale
+import speech
+
+class dateandtime():
+    locale.setlocale(locale.LC_ALL, '')
+    day = datetime.datetime.now().strftime("%d")
+    month = datetime.datetime.now().strftime("%m")
+    year = datetime.datetime.now().strftime("%Y")
+    hour = datetime.datetime.now().strftime("%H")
+    minute = datetime.datetime.now().strftime("%M")
+    date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+    dayname = datetime.datetime.now().strftime("%A")
+
+
+
+def all_voice_commands():    
+    while True:
+        text=speech.get_audio()
+        #swi
+        if "merhaba" in text:
+            speech.speak("Merhaba efendim.")
+        if "test" in text:
+            speech.speak("Test başarılı")
+        if "spotify aç" in text:
+            speech.speak("Spotify açılıyor.")
+            os.system("start spotify:")
+        if "youtube aç" in text:
+            speech.speak("Youtube açılıyor.")
+        if "sitemi aç" in text:
+            speech.speak("Siteniz açılıyor efendim")
+        if "sistemi kapat" in text:
+            speech.speak("Sistem kapatılıyor")
+            exit()
+        if "mail aç" in text:
+            speech.speak("Mail uygulamanız açılıyor.")
+            os.system("start outlookmail:")
+        if "saat kaç" in text:
+            speech.speak("Saat şu an "+str(dateandtime.hour+" "+dateandtime.minute))
+            print(str(dateandtime.hour+" "+dateandtime.minute))
+        if "teşekkürler" in text:
+            speech.speak("rica ederim efendim")
+            speech.wakeup()
+
+        if "bugün günlerden ne" in text:
+            speech.speak("Bugün günlerden "+str(dateandtime.dayname))
+        if "hangi yıldayız" in text:
+            speech.speak("Şu an "+str(dateandtime.year)+"yılındayız efendim.")
+        if "mail kapat" in text:
+            speech.speak("Tabii efendim")
+            #os.system("taskkill /IM HxOutlook.exe")
+            mail_task_kill=os.popen("taskkill /IM HxOutlook.exe /F").read()
+            print(mail_task_kill)
+            if mail_task_kill in 'ERROR: The process "HxOutlook.exe" not found.':
+                speech.speak("Uygulama zaten kapalı efendim.")
+        if "spotify kapat" in text:
+            speech.speak("tabii efendim")
+            spotify_task_kill=os.popen("taskkill /IM Spotify.exe /F").read()
+            if spotify_task_kill in "ERROR":
+                speech.speak("Uygulama zaten kapalı efendim")
+                
